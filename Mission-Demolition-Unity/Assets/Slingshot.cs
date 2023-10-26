fbfba0b0236efbf8dc1f291c4d78edc8882a4f0d
Sean McLaughlin <smm6050@utulsa.edu>
2023-10-26 11:45:50 -0500

    Added art assets
    camera settings
    lighting settings
    ground
    slingshot
    projectile

---
+++ Mission-Demolition-Unity/Assets/Slingshot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    void OnMouseEnter()
+    {
+        print("SS: OnMouseEnter()");
+    }
+
+    void OnMouseExit()
+    {
+        print("SS: OnMouseExit()");
+    }
+}
