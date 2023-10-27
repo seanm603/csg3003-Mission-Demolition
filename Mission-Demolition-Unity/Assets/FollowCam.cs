e32d1a145cae26c2ba7dc366fe4961a81fd674b7
seanm603 <68670448+seanm603@users.noreply.github.com>
2023-10-26 19:13:18 -0500

    add follow cam and cloud assets

---
+++ Mission-Demolition-Unity/Assets/FollowCam.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI; // static point of interest
+    [Header("Inscribed")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
+    [Header("Dynamic")]
+    public float camZ; // desired z pos of cam
+
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 destination = Vector3.zero;
+
+        if (POI != null)
+        {
+            Debug.Log("Getting POI rigidbody");
+            Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+            if ( (poiRigid != null) && (poiRigid.IsSleeping()))
+            {
+                Debug.Log("FC: Projectile stopped, returning to slingshot");
+                POI = null;
+            }
+        }
+
+        if (POI != null)
+        {
+            destination = POI.transform.position;
+        }
+
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+        transform.position = destination;
+
+    }
+}
--- Mission-Demolition-Unity/Assets/Slingshot.cs
+++ Mission-Demolition-Unity/Assets/Slingshot.cs
@@ -36,29 +36,29 @@ public class Slingshot : MonoBehaviour
         // print("SS: OnMouseExit()");
         launchPoint.SetActive(false);
     }
-    private void OnMouseDown()
+    void OnMouseDown()
     {
         aimingMode = true;
         projectile = Instantiate(projectilePrefab);
         projectile.transform.position = launchPos;
-    //    projectile.GetComponent<Rigidbody>.isKinematic = true;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 
     void Update()
     {
-        if (aimingMode || projectile != null) return;
+        if (!aimingMode) return;
 
         Vector3 mousePos2D = Input.mousePosition;
         mousePos2D.z = -Camera.main.transform.position.z;
         Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
         Vector3 mouseDelta = mousePos3D - launchPos;
 
-        //float maxMagnitude = this.GetComponent<SphereCollider>.radius;
-        //if (mouseDelta.magnitude > maxMagnitude)
-        //{
-        //    mouseDelta.Normalize();
-        //    mouseDelta *= maxMagnitude;
-        //}
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+           mouseDelta.Normalize();
+           mouseDelta *= maxMagnitude;
+        }
         Vector3 projPos = launchPos + mouseDelta;
         projectile.transform.position = projPos;
 
@@ -69,8 +69,9 @@ public class Slingshot : MonoBehaviour
             projRB.isKinematic = false;
             projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
             projRB.velocity = -mouseDelta * velocityMult;
+            Debug.Log("SS: projectile launched");
+            FollowCam.POI = projectile;
             projectile = null;
-
         }
     }
 
